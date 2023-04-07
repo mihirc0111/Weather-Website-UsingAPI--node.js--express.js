085d93a953f67a764cd437cf799d5f847a917bb3
MIHIR CHAVAN <mihirc0111@gmail.com>
2023-04-07 16:25:05 +0530

    3rd using cors=require

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const https = require("https");
 const bodyParser = require("body-parser");
+// ########################
+const cors = require('cors')
+// #######################
 require("dotenv").config();
 
 // Enable CORS for all origins
